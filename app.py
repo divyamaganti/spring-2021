3a4d0e3342734baa3665dae2e3153a47dfdae5d0
MadTBDiv <Dmcoding04>
2021-07-16 18:07:27 -0400

    Reverse Controller built and tested

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/i')
+def intro():
+    return 'Divya Maganti is currently learning python to obtain a decent programming career after she graduate' \
+           ' college.'
+
+
+@app.route('/b')
+def body():
+    return 'Divya Maganti is currently in high school and is aiming to attend either Rutgers University - New' \
+           'Brunswick, NYU, Stevens Institute of Technology or NJIT.'
+
+
+@app.route('/success')
+def ending():
+    return ''
+
+
+@app.route('/reverse')
+def reverse():
+    word = request.args.get('word')
+    # slicing
+    new_word = word[::-1]
+    return new_word
 
 
 if __name__ == '__main__':
--- test_app.py
+++ test_app.py
@@ -11,3 +11,21 @@ def client():
 def test_hello_world(client):
     response = client.get('/')
     assert b'Hello, World!' in response.data
+
+
+def test_intro(client):
+    response = client.get('/i')
+    assert b'Divya Maganti is currently learning python to obtain a decent programming career after she graduate' \
+           b' college.' in response.data
+
+
+def test_body(client):
+    response = client.get('/b')
+    assert b'Divya Maganti is currently in high school and is aiming to attend either Rutgers University - New' \
+           b'Brunswick, NYU, Stevens Institute of Technology or NJIT.' in response.data
+
+
+def test_reverse(client):
+    response = client.get('/reverse?word=buffalo')
+    assert b'olaffub' in response.data
+
